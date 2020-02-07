75515802caede1653736e217028a8dee691f9841
wsy <wsy1983wsy@163.com>
2020-02-07 16:36:19 +0800

    合并，压缩

--- project_build/grunt_test/Gruntfile.js
+++ project_build/grunt_test/Gruntfile.js
@@ -1,10 +1,32 @@
-module.exports = function(grunt){
+module.exports = function (grunt) {
     // 1. 初始化插件配置
     grunt.initConfig({
         //主要编码处
+        concat: {
+            options: {
+                separator: ';',
+            },
+            dist: {
+                src: ['src/js/*.js'],
+                dest: 'build/build.js',
+            },
+        },
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+                    '<%= grunt.template.today("yyyy-mm-dd") %> */'
+            },
+            build: {
+                files: {
+                    'build/build.min.js': ['build/build.js']
+                }
+            }
+        },
     });
     // 2. 加载插件任务
-    // grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
     // 3. 注册构建任务
     grunt.registerTask('default', []);
 };
