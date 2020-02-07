5d49155b23c2d844206917a5069357dd8567482b
wsy <wsy1983wsy@163.com>
2020-02-07 16:11:52 +0800

    初始化grunt

---
+++ project_build/grunt_test/Gruntfile.js
@@ -0,0 +1,10 @@
+module.exports = function(grunt){
+    // 1. 初始化插件配置
+    grunt.initConfig({
+        //主要编码处
+    });
+    // 2. 加载插件任务
+    // grunt.loadNpmTasks('grunt-contrib-concat');
+    // 3. 注册构建任务
+    grunt.registerTask('default', []);
+};
