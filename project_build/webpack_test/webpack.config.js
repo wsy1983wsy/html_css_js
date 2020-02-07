c0b4ebb05b957ceb6cc895a456152ba0d8056cb5
wsy <wsy1983wsy@163.com>
2020-02-07 23:55:42 +0800

    配置图片资源

--- project_build/webpack_test/webpack.config.js
+++ project_build/webpack_test/webpack.config.js
@@ -1,25 +1,26 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    entry: './src/js/entry.js',//入口文件的配置
+    entry: "./src/js/entry.js",//入口文件的配置
     output: {//输出配置
-        filename: 'bundle.js',//输出的文件名
-        path: path.resolve(__dirname, 'dist/js/')
+        filename: "bundle.js",//输出的文件名
+        publicPath: "js/",    //设置为index.html提供资源服务的时候具有强制性，不推荐使用
+        path: path.resolve(__dirname, "dist/js/")
     },
     module: {
         rules: [
             {
                 test: /\.css$/,
                 use: [
-                    'style-loader',
-                    'css-loader'
+                    "style-loader",
+                    "css-loader"
                 ]
             },
             {
                 test: /\.(png|jpg|gif)$/,
                 use: [
                     {
-                        loader: 'url-loader',
+                        loader: "url-loader",
                         options: {
                             limit: 8192
                         }
