8918013e8e95028e74bb2ee27f832d5c49cca34e
wsy <wsy1983wsy@163.com>
2020-02-09 19:00:24 +0800

    实现自定义的App.vue HelloWorld.vue

--- vue/VueDemo/src/App.vue
+++ vue/VueDemo/src/App.vue
@@ -1,15 +1,27 @@
 <template>
   <div>
-
+    <img src="./assets/logo.png" alt="" class="logo">
+    <!--使用组件-->
+    <HelloWorld/>
   </div>
 </template>
 
 <script>
+// 1. 引入组件
+import HelloWorld from './components/HelloWorld'
+
 export default {
-  name: 'App'
+  name: 'App',
+  // 映射组件标签
+  components: {
+    HelloWorld
+  }
 }
 </script>
 
 <style scoped>
-
+  .logo {
+    width: 200px;
+    height: 200px;
+  }
 </style>
--- vue/VueDemo/src/components/HelloWorld.vue
+++ vue/VueDemo/src/components/HelloWorld.vue
@@ -1,13 +1,23 @@
 <template>
-
+  <div>
+    <p>{{msg}}</p>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld'
+  // 配置对象（与Vue一致）
+  data () {
+    // 必须写函数
+    return {
+      msg: 'Hello Vue Component'
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+  p {
+    color: yellowgreen;
+  }
 </style>
