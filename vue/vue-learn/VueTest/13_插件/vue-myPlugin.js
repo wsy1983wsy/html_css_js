605554858309aee3236380c382c68f1c3a111708
wsy <wsy1983wsy@163.com>
2020-02-09 13:08:20 +0800

    自定义插件

--- vue/vue-learn/VueTest/13_插件/vue-myPlugin.js
+++ vue/vue-learn/VueTest/13_插件/vue-myPlugin.js
@@ -1,20 +1,31 @@
+
+/**Vue的插件库 */
 (function (window) {
-  const MyPlugin = {}
+  //需要向外暴露的插件对象
+  const MyPlugin = {};
   MyPlugin.install = function (Vue, options) {
     // 1. 添加全局方法或属性
     Vue.myGlobalMethod = function () {
-      console.log('Vue函数对象的myGlobalMethod()')
+      console.log("Vue函数对象的方法myGlobalMethod");
     }
 
     // 2. 添加全局资源
-    Vue.directive('my-directive',function (el, binding) {
-      el.textContent = 'my-directive----'+binding.value
+    Vue.directive('my-directive', function (el, binding) {
+      el.textContent = binding.value.toUpperCase();
+    })
+
+    // 3. 注入组件选项
+    Vue.mixin({
+      created: function () {
+
+      }
     })
 
     // 4. 添加实例方法
-    Vue.prototype.$myMethod = function () {
-      console.log('vm $myMethod()')
+    Vue.prototype.$myMethod = function (methodOptions) {
+      console.log("Vue的实例对象方法myMethod");
     }
-  }
-  window.MyPlugin = MyPlugin
-})(window)
+  };
+  //向外暴露
+  window.MyPlugin = MyPlugin;
+})(window);
