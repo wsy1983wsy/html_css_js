1d18e1e71f601570378f6f826cccd60167374e14
wsy <wsy1983wsy@163.com>
2020-02-15 07:39:36 +0800

    vue-resource 执行http请求

--- vue/VueDemo/src/04_ajax/App.vue
+++ vue/VueDemo/src/04_ajax/App.vue
@@ -15,7 +15,19 @@ export default {
     }
   },
   mounted () {
-    //发ajax请求获取数据
+    console.log('mounted')
+    // 发ajax请求获取数据
+    const url = 'https://api.github.com/search/repositories?q=v&sort=stars'
+    this.$http.get(url)
+      .then(response => {
+        console.log(response)
+        let result = response.data
+        const mostRepo = result.items[0]
+        this.repoUrl = mostRepo.html_url
+        this.repoName = mostRepo.name
+      }, error => {
+        alert('请求失败')
+      })
   }
 }
 </script>
--- vue/VueDemo/src/04_ajax/main.js
+++ vue/VueDemo/src/04_ajax/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
+import VueResource from 'vue-resource'
 import App from './App'
+
+// 声明使用插件
+// 给vm对象和组件对象添加一个属性$http
+Vue.use(VueResource)
 
 Vue.config.productionTip = false
 /* eslint-disable no-new */
