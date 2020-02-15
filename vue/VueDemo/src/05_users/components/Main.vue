a8f50619ae991de5943a412137e88f94de5aeb43
wsy <wsy1983wsy@163.com>
2020-02-15 08:28:08 +0800

    github search

--- vue/VueDemo/src/05_users/components/Main.vue
+++ vue/VueDemo/src/05_users/components/Main.vue
@@ -1,42 +1,64 @@
 <template>
-
-  <div class="row">
-    <div class="card">
-      <a href="https://github.com/reactjs" target="_blank">
-        <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style='width: 100px'/>
-      </a>
-      <p class="card-text">reactjs</p>
-    </div>
-    <div class="card">
-      <a href="https://github.com/reactjs" target="_blank">
-        <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style='width: 100px'/>
-      </a>
-      <p class="card-text">reactjs</p>
-    </div>
-    <div class="card">
-      <a href="https://github.com/reactjs" target="_blank">
-        <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style='width: 100px'/>
-      </a>
-      <p class="card-text">reactjs</p>
-    </div>
-    <div class="card">
-      <a href="https://github.com/reactjs" target="_blank">
-        <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style='width: 100px'/>
-      </a>
-      <p class="card-text">reactjs</p>
-    </div>
-    <div class="card">
-      <a href="https://github.com/reactjs" target="_blank">
-        <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style='width: 100px'/>
-      </a>
-      <p class="card-text">reactjs</p>
+  <div>
+    <h2 v-if="firstView">输入用户名搜索</h2>
+    <h2 v-if="loading">loading...</h2>
+    <h2 v-if="errorMsg">{{errorMsg}}</h2>
+    <div class="row">
+      <div class="card" v-for="(user,index) in users" :key="index">
+        <a :href="user.url" target="_blank">
+          <img :src="user.avatar_url" style='width: 100px'/>
+        </a>
+        <p class="card-text">{{user.name}}</p>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import PubSub from 'pubsub-js'
+import axios from 'axios'
+
 export default {
-  name: 'Main'
+  name: 'Main',
+  data () {
+    return {
+      firstView: true,
+      loading: false,
+      users: null,
+      errorMsg: ''
+    }
+  },
+  mounted () {
+    // 订阅消息
+    PubSub.subscribe('search', (msg, searchKey) => {
+      const url = `https://api.github.com/search/repositories?q=${searchKey}`
+      // 更新状态（请求中)
+      this.firstView = false
+      this.loading = true
+      this.users = null
+      this.errorMsg = ''
+      // 发ajax请求
+      axios.get(url)
+        .then(response => {
+          // 成功 更新状态（成功）
+          const result = response.data
+          console.log(result)
+          let users = result.items.map(item => {
+            return {
+              url: item.html_url,
+              avatar_url: item.owner.avatar_url,
+              name: item.name
+            }
+          })
+          this.loading = false
+          this.users = users
+        }).catch(error => {
+        // 失败 更新状态（失败）
+        this.loading = false
+        this.errorMsg = '请求失败: ' + error
+      })
+    })
+  },
 }
 </script>
 
--- vue/VueDemo/src/05_users/components/Search.vue
+++ vue/VueDemo/src/05_users/components/Search.vue
@@ -3,15 +3,32 @@
   <section class="jumbotron">
     <h3 class="jumbotron-heading">Search Github Users</h3>
     <div>
-      <input type="text" placeholder="enter the name you search"/>
-      <button>Search</button>
+      <input type="text" placeholder="enter the name you search" v-model="searchKey"/>
+      <button @click="handleSearch">Search</button>
     </div>
   </section>
 </template>
 
 <script>
+import PubSub from 'pubsub-js'
+
 export default {
-  name: 'Search'
+  name: 'Search',
+  data () {
+    return {
+      searchKey: ''
+    }
+  },
+  methods: {
+    handleSearch () {
+      const searchKey = this.searchKey.trim()
+      if (searchKey) {
+        //发布消息
+        PubSub.publish('search', searchKey)
+      }
+
+    }
+  }
 }
 </script>
 
