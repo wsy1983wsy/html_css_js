9a2a6b2643b7aa686e96223c85778c2101c1dca7
wsy <wsy1983wsy@163.com>
2020-02-15 22:19:50 +0800

    增加eslint，babel等插件

--- vue/vuex-demo/src/App.vue
+++ vue/vuex-demo/src/App.vue
@@ -1,10 +1,53 @@
 <template>
-    <div class="container">
-    </div>
+  <div class="container">
+    <p>click {{count}} times ,count is {{evenOrOdd}}</p>
+    <button @click="increment">+</button>
+    <button @click="reduce">-</button>
+    <button @click="incrementIfOdd">increment if odd</button>
+    <button @click="incrementAsync">increment async</button>
+  </div>
 </template>
 
 <script>
-    export default {}
+  export default {
+    name: "App",
+    data() {
+      return {
+        count: 0
+      }
+    },
+    computed: {
+      evenOrOdd() {
+        if (this.count % 2 == 0) {
+          return "偶数"
+        } else {
+          return "奇数";
+        }
+      }
+    },
+    methods: {
+      increment() {
+        let count = this.count;
+        this.count = count + 1;
+      },
+      reduce() {
+        if (this.count <= 0) {
+          return;
+        }
+        let count = this.count;
+        this.count = count - 1;
+      },
+      incrementIfOdd() {
+        let count = this.count;
+        if (count % 2 == 1) {
+          this.count = count + 1;
+        }
+      },
+      incrementAsync() {
+        setTimeout(this.increment, 1000);
+      }
+    }
+  }
 </script>
 
 <style>
--- vue/vuex-demo/src/main.js
+++ vue/vuex-demo/src/main.js
@@ -8,6 +8,6 @@ import App from './App.vue'
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  render: h => h(App),
+  render: h => h(App)
 })
 
