8f4267d819ec6fa25df573d90fe905da799f9309
wsy <wsy1983wsy@163.com>
2020-02-16 11:21:23 +0800

    调整方法调用

--- vue/vuex-demo/src/App.vue
+++ vue/vuex-demo/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <p>click {{$store.state.count}} times ,count is {{evenOrOdd}}</p>
+    <p>click {{count}} times ,count is {{evenOrOdd}}</p>
     <button @click="increment">+</button>
     <button @click="reduce">-</button>
     <button @click="incrementIfOdd">increment if odd</button>
@@ -9,30 +9,35 @@
 </template>
 
 <script>
+
+  import {mapState, mapGetters, mapActions} from 'vuex'
+
   export default {
     name: "App",
     computed: {
-      evenOrOdd() {
-        return this.$store.getters.evenOrOdd;
-      }
+      ...mapState(['count']), // mapState(['count'])返回值{count(){ return this.$store.state['count']}}
+      ...mapGetters(['evenOrOdd'])  // mapGetters(['evenOrOdd'])返回值{evenOrOdd(){ return this.$store.getters['evenOrOdd']}}
     },
     methods: {
-      increment() {
-        // 通知vuex去增加
-        // 触发增加事件
-        this.$store.dispatch('increment')
-      },
-      reduce() {
-
-        this.$store.dispatch('reduce')
-      },
-      incrementIfOdd() {
-        this.$store.dispatch('incrementIfOdd')
-      },
-      incrementAsync() {
-        this.$store.dispatch('incrementAsync')
-      }
+      ...mapActions(['increment', 'reduce', 'incrementIfOdd', 'incrementAsync'])
     }
+    // methods: {
+    // increment() {
+    //   // 通知vuex去增加
+    //   // 触发增加事件
+    //   this.$store.dispatch('increment')
+    // },
+    // reduce() {
+    //
+    //   this.$store.dispatch('reduce')
+    // },
+    // incrementIfOdd() {
+    //   this.$store.dispatch('incrementIfOdd')
+    // },
+    // incrementAsync() {
+    //   this.$store.dispatch('incrementAsync')
+    // }
+    // }
   }
 </script>
 
